c95e6bd63caa4a4e8e239638de98b8b7205bb183
Oz <58702127+ozjtech@users.noreply.github.com>
2020-07-11 23:48:44 -0500

    Add files via upload
    A lot has changed. outlines done, gotta add functionality for the menu items, spice up how it looks and all that

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 
 #import dependencies [X]
-#intergrate google sheets and drive APIS[]
-#build menu[]
+#intergrate google sheets and drive APIS[X]
+#build menu[X]
+#find out how to print a list of every sheet in the workspace[]
+#maybe by creating a variable that has a for loop of sheet1 - ??? idk it just needs to work    
+#create a function that generates a new submenu each time a new sheet is generated[]
+#findout hwo to get rid of all these unused import errors.
 import random
 import json
 import pprint
 import datetime
+from consolemenu import *
+from consolemenu.items import *
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
@@ -15,10 +21,46 @@ client = gspread.authorize(creds)
 
 #the user will be able to create new sheets for new lists
 # #because of this we are naming sheet1 primary sheet, so that its special
-primarySheet = client.open('underTakerDB').sheet1
-#simple pointer for our user to choose from
-choice = []
-#this function will act as our 'engine' that runs the problem until the user selects the 'exit' option in our menu
-def undertaking():
-    if(choice == 'end program'):
-        exit
+primarySheet = client.openall('underTakerDB')#.sheet1 
+
+#building the menu
+menu = ConsoleMenu("TheUnderTaker", "task organizer")
+
+
+#FUNCTIONALITY FOR THE MENU ITEMS GOES UNDER THIS LINE
+
+#creates a new spreadsheet
+def newList():
+    newSheetName = input("What would you like to call this list?")
+    client.open('underTakerDB').add_worksheet(newSheetName, 400,400)
+    newSheetConfirmation = "Your new tasklist %s is ready."
+    print(newSheetConfirmation % newSheetName)
+
+def showAllLists():
+    everyList = primarySheet.sheet
+    print(everyList)
+#NEW MENU ITEMS GO UNDER THIS LINE AFTER THEIR FUNCTIONALITY
+
+showAllLists()
+def startMenu():
+#create a submenu to show all sheets and let the user pick which one to print
+    viewTasksOption = FunctionItem("View all task lists", showAllLists)
+#create logic for adding tasks
+    addTaskOption = MenuItem("Add a task")
+#create logic for updating tasks
+    updateATaskOption = MenuItem("Update a task")
+#create logic for new tasks
+    newTaskListOption = FunctionItem("New Task list", newList)
+#create logic for deleting tasks
+    deleteATaskOption = MenuItem("Delete a task")
+#adding menu items to the menu
+    menu.append_item(viewTasksOption)
+    menu.append_item(addTaskOption)
+    menu.append_item(updateATaskOption)
+    menu.append_item(newTaskListOption)
+    menu.append_item(deleteATaskOption)
+#calling show to show the menu and allow the user to interact
+    menu.show()
+#commenting this out to test
+#startMenu()
+
